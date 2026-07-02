54df2bbc137b13e8e3916e47727d9fd0794bc209
KlismanAmiden <Wkklisman08@Gmail.com>
2026-07-02 15:50:28 -0300

    Separei as pastas e adc a pasta backand

---
+++ backend/src/test/java/com/Barbearia/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barbearia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f84d7549d4aa30c0831b67fe83dac9c43e7a9b44
KlismanAmiden <Wkklisman08@Gmail.com>
2026-08-08 17:27:57 -0300

    Acesso liberado para o FrontEnd

--- backend/src/main/java/com/Barbearia/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/Barbearia/backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.Barbearia.backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.Barbearia.backend.security.JwtAuthFilter;
 
@@ -30,6 +35,7 @@ public PasswordEncoder passwordEncoder() {
       @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
@@ -40,5 +46,30 @@ public PasswordEncoder passwordEncoder() {
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); 
+
+        configuration.setAllowedMethods(List.of(
+            "GET", 
+            "POST",
+            "PUT",
+            "DELETE", 
+            "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of(
+            "Authorization", 
+            "Content-Type"
+        ));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
